4c9dce12e4e1830412bc27b7283cfb77ae7658e1
PiligalC0D3 <michelpiligal@gmail.com>
2024-11-14 00:53:07 -0300

    Primeiro Comit

---
+++ Netflix/scripts.js
@@ -0,0 +1,17 @@
+/* 
+  - Variável = UM PEDACINHO DO COMPUTADOR, QUE EU POSSO GUARDAR OQUE EU QUISER 
+  - Funções 
+      Pedacinho de código que só executa, quando é chamado
+*/
+
+    let botaoSom = document.querySelector(".botao-som")
+    let video = document.querySelector(video)
+    botaoSom.addEventListener("click" , ligaSom)
+
+    function ligaSom (){
+        alert("Opa, o botão foi clicado")
+    } 
+
+    mostrarAlerta(abacate)
+
+    
